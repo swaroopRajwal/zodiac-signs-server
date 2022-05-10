3e4c1568810531134c77baf549290acccc8fcf35
swaroopRajwal <swaroop.rajwal18@gmail.com>
2022-05-10 21:45:47 +0530

    🚀 improved error handling

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+// * importing external libraries
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 
 // * importing routers
 const signsRouter = require("./routes/signs.router");
 const datesRouter = require("./routes/date.router");
+
+// * setting up the middlewares
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
   console.log(`server started on port ${process.env.PORT}`);
@@ -12,3 +17,6 @@ app.listen(process.env.PORT, () => {
 
 app.use("/signs", signsRouter);
 app.use("/date", datesRouter)
+app.get("*", (req, res) => {
+  res.status(404).send({message: "Route not found"});
+})
