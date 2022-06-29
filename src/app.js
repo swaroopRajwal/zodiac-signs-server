73f943e540b58a0504f0e4042329fd831924d3f9
swaroopRajwal <swaroop.rajwal18@gmail.com>
2022-06-29 15:25:31 +0530

    ♻️ changed static directory to public

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ const cors = require("cors");
 // * importing routers
 const signsRouter = require("./routes/signs.router");
 const datesRouter = require("./routes/date.router");
+const imageRouter = require("./routes/image.router");
 
 // * setting up the middlewares
 app.use(cors());
+app.use(express.static('public'));
 
 // * starting the server
 const PORT = process.env.PORT || 8000;
@@ -17,8 +19,10 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 })
 
+// * setting up routers
 app.use("/signs", signsRouter);
 app.use("/date", datesRouter);
+app.use("/image", imageRouter);
 
 app.get("*", (req, res) => {
   res.status(404).send({message: "Route not found"});
